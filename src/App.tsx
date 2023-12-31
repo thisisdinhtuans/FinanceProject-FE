52a81b8c157078d0eb4e03d59d8cb2e3b228ded6
Dinh Tuans <dinhmanhtuans89@gmail.com>
2023-12-31 10:54:05 +0700

    Forms

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,18 @@ function App() {
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
   const [serverError, setServerError] =useState<String>()
 
-  const handleChange=(e:ChangeEvent<HTMLInputElement>)=> {
+  const handleSearchChange=(e:ChangeEvent<HTMLInputElement>)=> {
       setSearch(e.target.value);
       console.log(e);
   };
-  const onClick = async (e:SyntheticEvent) => {
+
+  const onPortfolioCreate = (e:SyntheticEvent) => {
+    e.preventDefault();
+    console.log(e);
+  }
+
+  const onSearchSubmit = async (e:SyntheticEvent) => {
+    e.preventDefault();
       const result = await searchCompanies(search);
       if(typeof result ==="string") {
         setServerError(result);
@@ -27,9 +34,9 @@ function App() {
   }; 
   return (
     <div className="App">
-      <Search onClick={onClick} search={search} handleChange={handleChange}/>
+      <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange}/>
       {serverError&& <h1>{serverError}</h1>}
-      <CardList searchResults={searchResult}/>
+      <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate}/>
       {serverError && <div>Unable to connect to API</div>}
     </div>
   );
