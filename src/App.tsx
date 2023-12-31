a966733f4a03acf8e9a7f982b6d11219e8251c1c
Dinh Tuans <dinhmanhtuans89@gmail.com>
2023-12-31 09:01:22 +0700

    Type Narrowing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { ChangeEvent, SyntheticEvent, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Card from './Components/Card/Card';
 import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
+import { searchCompanies } from './api';
+import { CompanySearch } from './company';
 
 function App() {
+  const [search, setSearch]=useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  const [serverError, setServerError] =useState<String>()
+
+  const handleChange=(e:ChangeEvent<HTMLInputElement>)=> {
+      setSearch(e.target.value);
+      console.log(e);
+  };
+  const onClick = async (e:SyntheticEvent) => {
+      const result = await searchCompanies(search);
+      if(typeof result ==="string") {
+        setServerError(result);
+      } else if(Array.isArray(result.data)) {
+        setSearchResult(result.data);
+      }
+      console.log(searchResult);
+  }; 
   return (
     <div className="App">
-      <Search />
+      <Search onClick={onClick} search={search} handleChange={handleChange}/>
       <CardList />
     </div>
   );
