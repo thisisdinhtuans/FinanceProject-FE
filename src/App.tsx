38b6d9a0e5596be42fb041b62e2d07bb33e1f77a
Dinh Tuans <dinhmanhtuans89@gmail.com>
2023-12-31 11:38:47 +0700

    Arrays

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,29 @@ import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
 import { searchCompanies } from './api';
 import { CompanySearch } from './company';
+import ListPortfolio from './Components/Portfolio/ListPortfolio/ListPortfolio';
 
 function App() {
   const [search, setSearch]=useState<string>("");
+  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-  const [serverError, setServerError] =useState<String>()
+  const [serverError, setServerError] =useState<String>();
 
   const handleSearchChange=(e:ChangeEvent<HTMLInputElement>)=> {
       setSearch(e.target.value);
       console.log(e);
   };
 
-  const onPortfolioCreate = (e:SyntheticEvent) => {
+  const onPortfolioCreate = (e:any) => {
     e.preventDefault();
-    console.log(e);
+    const exits=portfolioValues.find((value=>value === e.target[0].value))
+    if(exits) return;
+    const updatedPortfolio=[...portfolioValues, e.target[0].value];
+    setPortfolioValues(updatedPortfolio);
   }
 
   const onSearchSubmit = async (e:SyntheticEvent) => {
-    e.preventDefault();
+      e.preventDefault();
       const result = await searchCompanies(search);
       if(typeof result ==="string") {
         setServerError(result);
@@ -36,6 +41,7 @@ function App() {
     <div className="App">
       <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange}/>
       {serverError&& <h1>{serverError}</h1>}
+      <ListPortfolio portfolioValues={portfolioValues}/>
       <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate}/>
       {serverError && <div>Unable to connect to API</div>}
     </div>
