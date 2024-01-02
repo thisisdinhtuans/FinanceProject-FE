3cdd281aa040fdb1e53afb005700ee30343a026f
Dinh Tuans <dinhmanhtuans89@gmail.com>
2024-01-02 09:42:35 +0700

    Conparable Finder

--- src/Pages/CompanyPage/CompanyPage.tsx
+++ src/Pages/CompanyPage/CompanyPage.tsx
@@ -6,6 +6,7 @@ import Sidebar from '../../Components/Sidebar/Sidebar';
 import CompanyDashboard from '../../Components/CompanyDashboard/CompanyDashboard';
 import Tile from '../../Components/Tile/Tile';
 import Spinner from '../../Components/Spinner/Spinner';
+import CompFinder from '../../Components/CompFinder/CompFinder';
 
 interface Props {}
 
@@ -30,7 +31,8 @@ function CompanyPage({}: Props) {
           <Tile title="Company Name" subTitle={company.companyName}></Tile>
           <Tile title="Price" subTitle={company.price.toString()}></Tile>
           <Tile title="Sector" subTitle={company.sector}></Tile>
-          <Tile title="DCF" subTitle={company.dcf.toString()}></Tile>
+          <Tile title="Market Cap" subTitle={company.mktCap.toString()}></Tile>
+          <CompFinder ticker={company.symbol} />
           <p className="bg-white shadow rounded text-medium text-gray-900 p-3 mt-1 m-4">
             {company.description}
           </p>
--- src/api.tsx
+++ src/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { CompanyBalanceSheet, CompanyCashFlow, CompanyIncomeStatement, CompanyKeyMetrics, CompanyProfile, CompanySearch } from "./company"
+import { CompanyBalanceSheet, CompanyCashFlow, CompanyCompData, CompanyIncomeStatement, CompanyKeyMetrics, CompanyProfile, CompanySearch } from "./company"
 interface SearchResponse {
     data: CompanySearch[];
 }
@@ -74,3 +74,25 @@ export const getCompanyProfile = async (query: string) => {
       console.log("error message: ", error.message);
     }
   }; 
+
+  export const getCompData = async (query: string) => {
+    try {
+      const data = await axios.get<CompanyCompData[]>(
+        `https://financialmodelingprep.com/api/v4/stock_peers?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
+      );
+      return data;
+    } catch (error: any) {
+      console.log("error message: ", error.message);
+    }
+  };
+
+  export const getTenK = async (query: string) => {
+    try {
+      const data = await axios.get<CompanyTenK[]>(
+        `https://financialmodelingprep.com/api/v4/stock_peers?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
+      );
+      return data;
+    } catch (error: any) {
+      console.log("error message: ", error.message);
+    }
+  };
