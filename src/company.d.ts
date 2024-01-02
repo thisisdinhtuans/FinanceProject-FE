2a42938b223b2e1c0bf70c031466ff75102a12e8
Dinh Tuans <dinhmanhtuans89@gmail.com>
2024-01-02 12:49:23 +0700

    10-K Finder

--- src/company.d.ts
+++ src/company.d.ts
@@ -313,3 +313,12 @@ export interface CompanySearch {
     symbol: string;
     peersList:string[];
   }
+  export interface CompanyTenK {
+    symbol: string;
+    fillingDate:string;
+    acceptedDate:string;
+    cik: string;
+    type:string;
+    link:string;
+    finalLink: string;
+  }
