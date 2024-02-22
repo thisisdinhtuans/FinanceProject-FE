6e30184da91094de5fe65d4de69a007d0589069f
Dinh Tuans <dinhmanhtuans89@gmail.com>
2024-02-22 12:32:42 +0700

    Comment Get

--- src/Components/StockComment/StockComment.tsx
+++ src/Components/StockComment/StockComment.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import StockCommentForm from './StockCommentForm/StockCommentForm';
-import { commentPostAPI } from '../../Services/CommentService';
+import { commentGetAPI, commentPostAPI } from '../../Services/CommentService';
 import { toast } from 'react-toastify';
+import { CommentGet } from '../../Models/Comment';
+import StockCommentList from '../StockCommentList/StockCommentList';
+import Spinner from '../Spinners/Spinner';
 
 type Props = {
     stockSymbol:string;
@@ -13,17 +16,40 @@ type CommentFormInputs = {
 };
 
 const StockComment = ({stockSymbol}: Props) => {
+    //Lưu trữ trạng thái của mình bên trong state   
+    const [comments, setComment]=useState<CommentGet[]|null>(null);
+    const [loading, setLoading] = useState<boolean>();
+
+
+    useEffect(() => {
+        getComments();
+        //[] là phải đặt 1 mảng thực tế vào đây
+    }, []);
+
     const handleComment = (e: CommentFormInputs) => {
         commentPostAPI(e.title, e.content, stockSymbol).then((res)=>{
             if(res) {
                 toast.success("Comment created successfully!")
+                getComments();
             }
         }).catch((e)=>{
             toast.warning(e);
         })
     }
+    const getComments =() => {
+        setLoading(true);
+        //truyền symbol chứng khoán cho nó
+        commentGetAPI(stockSymbol)
+        .then((res) =>{
+            setLoading(false);
+            setComment(res?.data!)
+        });
+    }
   return (
-    <StockCommentForm symbol={stockSymbol} handleComment={handleComment}/>
+    <div className="flex flex-col">
+        {loading ?<Spinner /> :<StockCommentList comments={comments!}/>}
+        <StockCommentForm symbol={stockSymbol} handleComment={handleComment}/>
+    </div>
   )
 }
 export default StockComment;
--- src/Services/CommentService.tsx
+++ src/Services/CommentService.tsx
@@ -1,14 +1,26 @@
 import axios from "axios";
-import { CommentPost } from "../Models/Comment";
+import { CommentGet, CommentPost } from "../Models/Comment";
 import { handlerError } from "../Helpers/ErrorHandler";
 
 const api="http://localhost:5296/api/comment/";
 export const commentPostAPI=async (title:string, content:string, symbol:string) => {
     try {
-        const data=await axios.post<CommentPost>(api+`${symbol}`,{
+        const data=await axios.post<CommentPost>(api+`${symbol}`,
+        //đây là đầu vào
+        {
             title:title,
             content:content,
-        })
+        }
+        )
+        return data;
+    } catch(error) {
+        handlerError(error);
+    }
+}
+
+export const commentGetAPI=async (symbol:string) => {
+    try {
+        const data=await axios.get<CommentGet[]>(api+`?Symbol=${symbol}`);
         return data;
     } catch(error) {
         handlerError(error);
